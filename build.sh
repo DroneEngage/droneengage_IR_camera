91ac65d2e05464d2843c819e0dec263edaacad30
HefnySco <mohammad.hefny@gmail.com>
2026-01-19 23:38:24 +0300

    updated build command

--- build.sh
+++ build.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+
+# Default build type is DEBUG
+BUILD_TYPE="DEBUG"
+ADDITIONAL_DEFINES=""
+
+# Check for RELEASE parameter
+if [ "$1" = "RELEASE" ]; then
+    BUILD_TYPE="RELEASE"
+    shift  # Remove RELEASE from arguments
+fi
+
+# Add any additional defines from remaining parameters
+for arg in "$@"; do
+    ADDITIONAL_DEFINES="$ADDITIONAL_DEFINES -D$arg"
+done
+
 rm -rf ./build
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=DEBUG ../
+cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON $ADDITIONAL_DEFINES ../
 make
 
